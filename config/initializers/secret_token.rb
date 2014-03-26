896cb7db592f2505fab25cd7f8dd998cf0eef73c
Phil Nguyen <pnguyen06@gmail.com>
2014-03-25 20:36:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Philnguyenme::Application.config.secret_key_base = '3eff493c0816caff2c54e213382cf5810c264aa42d45b3ea76bdbdb33b05bc7ae1ea814c069e0a2faaaf3bb042c5a2ce1215da87850b2f5f8923995944b92d4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Philnguyenme::Application.config.session_store :cookie_store, key: '_philnguyenme_session'
